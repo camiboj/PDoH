508f1c962eb339ac1e29f9fb10e09c61dab7a6f7
CamiBoj <camiboj@gmail.com>
2022-03-29 18:04:14 -0300

    minor fix

--- app/src/main/java/com/mocyx/basic_client/MainActivity.java
+++ app/src/main/java/com/mocyx/basic_client/MainActivity.java
@@ -13,7 +13,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;F
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
