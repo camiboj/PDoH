70630b9d5d1d975e48bd8d3c050aa5a2606179ed
chortas <chortas@fi.uba.ar>
2022-10-04 23:41:00 -0300

    Add more dns requesters

--- app/src/main/java/com/tpp/private_doh/controller/ShardingControllerFactory.java
+++ app/src/main/java/com/tpp/private_doh/controller/ShardingControllerFactory.java
@@ -8,6 +8,7 @@ import com.tpp.private_doh.util.Requester;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShardingControllerFactory {
 
@@ -16,21 +17,23 @@ public class ShardingControllerFactory {
     private final ShardingController hybridDnsShardingController;
 
     public ShardingControllerFactory() {
-        PublicDnsRequester publicDnsRequester = new PublicDnsRequester("8.8.8.8");
-        PublicDnsRequester publicDnsRequester2 = new PublicDnsRequester("8.8.8.8");
-        PublicDnsRequester publicDnsRequester3 = new PublicDnsRequester("8.8.8.8");
+        List<String> pureDnsResolvers = new ArrayList<>();
+        pureDnsResolvers.add("208.67.222.222");
+        pureDnsResolvers.add("208.67.220.220");
+        pureDnsResolvers.add("1.1.1.1");
+        pureDnsResolvers.add("1.0.0.1");
+        pureDnsResolvers.add("8.8.8.8");
+        pureDnsResolvers.add("8.8.4.4");
+        pureDnsResolvers.add("9.9.9.9");
+        pureDnsResolvers.add("149.112.112.112");
 
         DoHRequester googleDohRequester = new GoogleDoHRequester();
         DoHRequester cloudflareDohRequester = new CloudflareDoHRequester();
         DoHRequester quad9DohRequester = new CloudflareDoHRequester();
 
-        List<Requester> pureDnsRequesters = new ArrayList<>();
+        List<Requester> pureDnsRequesters = pureDnsResolvers.stream().map(PublicDnsRequester::new).collect(Collectors.toList());
         List<Requester> pureDohRequesters = new ArrayList<>();
         List<Requester> hybridDnsRequesters = new ArrayList<>();
-
-        pureDnsRequesters.add(publicDnsRequester);
-        pureDnsRequesters.add(publicDnsRequester2);
-        pureDnsRequesters.add(publicDnsRequester3);
 
         pureDohRequesters.add(googleDohRequester);
         pureDohRequesters.add(cloudflareDohRequester);
