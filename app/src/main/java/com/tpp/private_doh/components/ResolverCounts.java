f61740ac5913d7bc2b3be630296d54c6b8aa03e6
CamiBoj <camiboj@gmail.com>
2022-10-24 22:40:38 +0100

    fe refactor

---
+++ app/src/main/java/com/tpp/private_doh/components/ResolverCounts.java
@@ -0,0 +1,13 @@
+package com.tpp.private_doh.components;
+
+import android.content.Context;
+import android.widget.FrameLayout;
+
+public class ResolverCounts extends FrameLayout {
+
+    public ResolverCounts(Context context) {
+        super(context);
+    }
+
+
+}
